d21b577e95881c1c3140276b8a4012150e225b59
Emily Yu <eyudeveloper@gmail.com>
2021-01-30 01:10:58 -0800

    pull images from vidyas

---
+++ chrome-extension/img.js
@@ -0,0 +1,25 @@
+
+console.log('asdf')
+
+// get a snapshot from videos that are being displayed
+videoCandidates = document.getElementsByClassName('Gv1mTb-aTv5jf')
+for (let video of videoCandidates) {
+    console.log(video.style.display)
+    if (video.style.display == "") { // process
+
+        const canvas = document.createElement("canvas");
+        // scale the canvas accordingly
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        // draw the video at that frame
+        canvas.getContext('2d')
+          .drawImage(video, 0, 0, canvas.width, canvas.height);
+        // convert it to a usable data URL
+        const dataURL = canvas.toDataURL();
+
+        var img = document.createElement("img");
+        img.src = dataURL;
+
+        console.log(img) // image in base64URL
+    }
+}
